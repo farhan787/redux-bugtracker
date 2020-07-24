866208fdb339d8c623e4e73f3a6828b521ba4495
Muhammad Farhan <farhanshah.96930012@gmail.com>
2020-07-24 20:21:00 +0530

    bug tracker using redux setup added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('Hello World!');
+import store from './store';
+import { bugAdded, bugRemoved, bugResolved } from './actions';
+
+// state = reducer(state, action)
+// notify the subscribers
+
+// to prevent memory leaks
+const unsubscribe = store.subscribe(() => {
+  console.log('Store changed!', store.getState());
+});
+
+store.dispatch(bugAdded('Bug 1'));
+store.dispatch(bugResolved(1));
+
+// UI can unsbscribe from state change of store
+unsubscribe();
+
+store.dispatch(bugRemoved(1));
+
+console.log(store.getState());
